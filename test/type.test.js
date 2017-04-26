3677d06cfd1b1240a853ae3e33d31027b2c3e4fe
blade254353074 <blade254353074@hotmail.com>
2017-04-26 11:08:05 +0800

    Improve test case

--- test/type.test.js
+++ test/type.test.js
@@ -1,8 +1,12 @@
+import { JSDOM } from 'jsdom'
 import UrlScheme from '../src/url-scheme'
 import CancelToken from '../src/CancelToken'
 import Cancel from '../src/Cancel'
 
-test('CancelToken is work fine', done => {
+const { window } = new JSDOM()
+jest.useFakeTimers()
+
+test('CancelToken is work fine', () => {
   const source = CancelToken.source()
 
   expect(source).toEqual({
@@ -17,7 +21,77 @@ test('CancelToken is work fine', done => {
       expect(reason).toBeInstanceOf(Cancel)
       expect(reason.message).toBe('foo')
       expect(reason.toString()).toBe('Cancel: foo')
-      done()
     })
-    .catch(done.fail)
+})
+
+describe('Timeout checking', () => {
+  test('Waits 2.048 second timeout', () => {
+    new UrlScheme({
+      url: 'scheme://foo/bar?baz=true',
+      timeout: 2048
+    })
+
+    expect(setTimeout.mock.calls.length).toBe(1)
+    expect(setTimeout.mock.calls[0][1]).toBe(2048)
+  })
+
+  test('Timeout error throw is right', () => {
+    new UrlScheme({
+      url: 'scheme://foo/bar?baz=true',
+      timeout: 1000
+    })
+      .catch(err => {
+        expect(setTimeout.mock.calls.length).toBe(2)
+        expect(setTimeout.mock.calls[0][2]).toBe(1000)
+        const error = new Error('URL Scheme request timeout.')
+        error.type = 'timeout'
+        expect(err).toEqual(error)
+      })
+  })
+
+  test('Never timeout', () => {
+    expect(_ => {
+      jest.useFakeTimers()
+      new UrlScheme({
+        url: 'scheme://foo/bar?baz=true',
+        timeout: 0
+      })
+      expect(setTimeout.mock.calls.length).toBe(0)
+    }).not.toThrow()
+  })
+})
+
+describe('Type checking', () => {
+  test('param url checking is work fine', () => {
+    expect(_ => {
+      new UrlScheme({ url: true })
+    }).toThrow('url must be a string.')
+    expect(_ => {
+      new UrlScheme({ url: { path: 'foo://bar' } })
+    }).toThrow('url must be a string.')
+    expect(_ => {
+      new UrlScheme({ url: /foo:\/\/bar/g })
+    }).toThrow('url must be a string.')
+    expect(_ => {
+      new UrlScheme({ url: '' })
+    }).toThrow('url cannot be empty string.')
+    expect(_ => {
+      new UrlScheme({ url: 'foo://bar' })
+    }).not.toThrow()
+  })
+
+  test('param query checking is work fine', () => {
+    expect(_ => {
+      new UrlScheme({
+        url: 'scheme://foo/bar?baz=true',
+        query: ''
+      })
+    }).toThrow('query must be an object.')
+    expect(_ => {
+      new UrlScheme({
+        url: 'scheme://foo/bar?baz=true',
+        query: ''
+      })
+    }).toThrow('query must be an object.')
+  })
 })
